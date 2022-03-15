0f1873806eeed39e1f3309ca3f332f5fa8f6333f
masonhorder <masonhorder@gmail.com>
2022-03-14 22:29:10 -0700

    working on adding firebase backend

---
+++ schoolloop/android/app/src/main/kotlin/com/example/schoolloop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.schoolloop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ schoolloop/lib/functions.dart
@@ -0,0 +1,4 @@
+getGrades(String domain, String password, String username){
+  // function to somehow get a list of grades in format of: [[Period Number, Class Name, Teacher's Name, Grade (percentage), Grade Letter]]
+  return [[1, "Math", "Pangition", 96.78, "A"], [1, "Biology", "Richman", null, null]];
+}
